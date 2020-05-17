20429d6bbb6a49a331fca810f7ae89d2d3a3bf90
Jay Butera <buterajay@protonmail.com>
2020-05-17 13:26:39 -0500

    Just something useful I think?

--- script.js
+++ script.js
@@ -223,6 +223,7 @@ class Scene {
     }
 
     mouseDownEvent = (e) => {
+        //document.getElementById('debug').innerText = "mouse down";
         // tell the browser we're handling this mouse event
         e.preventDefault();
         e.stopPropagation();
@@ -235,6 +236,8 @@ class Scene {
     }
 
     touchDownEvent = (e) => {
+        //document.getElementById('debug').innerText = "touch down";
+
         // tell the browser we're handling this touch event
         e.preventDefault();
         e.stopPropagation();
